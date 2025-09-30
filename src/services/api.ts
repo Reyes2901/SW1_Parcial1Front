6bdf0035889ebed66bf38fdfbe07a26e2c1910af
Reyes2901 <reyesponce78@gmail.com>
2025-09-30 12:28:08 -0600

    deploy front 1.0

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,16 @@
 import axios from 'axios';
-
+/*
 const api = axios.create({
   baseURL: 'http://localhost:8000/api/',  // URL base del backend
   withCredentials: true,                  
 });
 
-export default api;
+export default api;*/
+const API_URL = process.env.REACT_APP_API_URL;
+
+export const getProjects = async () => {
+  const response = await fetch(`${API_URL}/api/projects/`);
+  if (!response.ok) throw new Error("Error fetching projects");
+  return response.json();
+};
+
